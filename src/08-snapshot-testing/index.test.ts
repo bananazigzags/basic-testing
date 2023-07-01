85d15772014b6eaa952b88d963318e49f2149ae1
Maria <bananazigzags@gmail.com>
2023-07-01 21:07:03 +0300

    feat: add 08-snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,28 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const list = generateLinkedList<number>([5, 6, 123]);
+    expect(list).toStrictEqual({
+      value: 5,
+      next: {
+        value: 6,
+        next: {
+          value: 123,
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const list = generateLinkedList<number>([5, 6, 123]);
+    expect(list).toMatchSnapshot();
   });
 });
